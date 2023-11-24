8f785f87b31a860b2e82125971ad557d2c51a8f8
Weongyo Jeong <weongyo@3rocks.net>
2023-11-24 11:44:45 -0800

    Updates the filter rule.

--- res/content_scripts/cs_all.js
+++ res/content_scripts/cs_all.js
@@ -2103,3 +2103,55 @@ chrome.storage.local.get(mudfish_adclean_g_conf_102ab386d5a9dff88c210774d3fa0abb
     });
   }
 });
+
+
+function mudfish_adclean_0e16d6892416fcb8dd01d4990dc0fb4f() {
+  try {
+    Sizzle(`amp-ad`).forEach(element => {
+      element.style.display = "none";
+    });
+  } catch (error) {
+    console.log('[ERROR] mudfish_adclean rule error: ' + error);
+  }
+}
+
+var mudfish_adclean_g_conf_0e16d6892416fcb8dd01d4990dc0fb4f = {
+  mudfish_adclean_filter_on: true
+};
+chrome.storage.local.get(mudfish_adclean_g_conf_0e16d6892416fcb8dd01d4990dc0fb4f, function (items) {
+  if (mudfish_adclean_g_conf_0e16d6892416fcb8dd01d4990dc0fb4f.mudfish_adclean_filter_on) {
+    const observer = new MutationObserver(() => {
+      mudfish_adclean_0e16d6892416fcb8dd01d4990dc0fb4f();
+    });
+    observer.observe(document, { childList: true, subtree: true });
+    window.addEventListener("load", (event) => {
+      mudfish_adclean_0e16d6892416fcb8dd01d4990dc0fb4f();
+    });
+  }
+});
+
+
+function mudfish_adclean_5c311976776f8fdcef742dce354b1d65() {
+  try {
+    Sizzle(`iframe[src*="//authanalysis.com/"]`).forEach(element => {
+      element.style.display = "none";
+    });
+  } catch (error) {
+    console.log('[ERROR] mudfish_adclean rule error: ' + error);
+  }
+}
+
+var mudfish_adclean_g_conf_5c311976776f8fdcef742dce354b1d65 = {
+  mudfish_adclean_filter_on: true
+};
+chrome.storage.local.get(mudfish_adclean_g_conf_5c311976776f8fdcef742dce354b1d65, function (items) {
+  if (mudfish_adclean_g_conf_5c311976776f8fdcef742dce354b1d65.mudfish_adclean_filter_on) {
+    const observer = new MutationObserver(() => {
+      mudfish_adclean_5c311976776f8fdcef742dce354b1d65();
+    });
+    observer.observe(document, { childList: true, subtree: true });
+    window.addEventListener("load", (event) => {
+      mudfish_adclean_5c311976776f8fdcef742dce354b1d65();
+    });
+  }
+});
